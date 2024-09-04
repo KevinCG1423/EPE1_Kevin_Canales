3ff057ad148c3242b7673cd507361e0bdba07121
KevinCG1423 <kevin.canales.gonzalez@gmail.com>
2024-09-04 15:52:40 -0400

    Agregar archivos de proyecto.

---
+++ EPE1_Kevin_Canales/Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        cmbProductos = New ComboBox()
+        btnCargar = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        txtDescripcion = New TextBox()
+        txtPrecio = New TextBox()
+        txtStock = New TextBox()
+        SuspendLayout()
+        ' 
+        ' cmbProductos
+        ' 
+        cmbProductos.FormattingEnabled = True
+        cmbProductos.Items.AddRange(New Object() {"Té Dharamsala", "Cerveza tibetana Barley", "Sirope de regaliz", "Especias Cajun del chef Anton", "Mezcla Gumbo del chef Anton", "Mermelada de grosellas de la abuela", "Peras secas orgánicas del tío Bob", "Salsa de arándanos Northwoods", "Buey Mishi Kobe", "Pez espada", "Queso Cabrales", "Queso Manchego La Pastora", "Algas Konbu", "Cuajada de judías", "Salsa de soja baja en sodio", "Postre de merengue Pavlova", "Cordero Alice Springs", "Langostinos tigre Carnarvon", "Pastas de té de chocolate", "Mermelada de Sir Rodney's", "Bollos de Sir Rodney's", "Pan de centeno crujiente estilo Gustaf's", "Pan fino", "Refresco Guaraná Fantástica", "Crema de chocolate y nueces NuNuCa", "Ositos de goma Gumbär", "Chocolate Schoggi", "Col fermentada Rössle", "Salchicha Thüringer", "Arenque blanco del noroeste", "Queso gorgonzola Telino", "Queso Mascarpone Fabioli", "Queso de cabra", "Cerveza Sasquatch", "Cerveza negra Steeleye", "Escabeche de arenque", "Salmón ahumado Gravad", "Vino Côte de Blaye", "Licor verde Chartreuse", "Carne de cangrejo de Boston", "Crema de almejas estilo Nueva Inglaterra", "Tallarines de Singapur", "Café de Malasia", "Azúcar negra Malacca", "Arenque ahumado", "Arenque salado", "Galletas Zaanse", "Chocolate holandés", "Regaliz", "Chocolate blanco", "Manzanas secas Manjimup", "Cereales para Filo", "Empanada de carne", "Empanada de cerdo", "Paté chino", "Gnocchi de la abuela Alicia", "Raviolis Angelo", "Caracoles de Borgoña", "Raclet de queso Courdavault", "Camembert Pierrot", "Sirope de arce", "Tarta de azúcar", "Sandwich de vegetales", "Bollos de pan de Wimmer", "Salsa de pimiento picante de Luisiana", "Especias picantes de Luisiana", "Cerveza Laughing Lumberjack", "Barras de pan de Escocia", "Queso Gudbrandsdals", "Cerveza Outback", "Crema de queso Fløtemys", "Queso Mozzarella Giovanni", "Caviar rojo", "Queso de soja Longlife", "Cerveza Klosterbier Rhönbräu", "Licor Cloudberry", "Salsa verde original Frankfurter"})
+        cmbProductos.Location = New Point(259, 127)
+        cmbProductos.Name = "cmbProductos"
+        cmbProductos.Size = New Size(216, 28)
+        cmbProductos.TabIndex = 0
+        ' 
+        ' btnCargar
+        ' 
+        btnCargar.Location = New Point(267, 42)
+        btnCargar.Name = "btnCargar"
+        btnCargar.Size = New Size(177, 29)
+        btnCargar.TabIndex = 1
+        btnCargar.Text = "Cargar Productos"
+        btnCargar.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(81, 42)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(159, 20)
+        Label1.TabIndex = 2
+        Label1.Text = "boton  de cargar c.box"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(55, 135)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(148, 20)
+        Label2.TabIndex = 3
+        Label2.Text = "seleccionar producto"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(55, 216)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(145, 20)
+        Label3.TabIndex = 4
+        Label3.Text = "detalle del producto"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(136, 280)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(104, 20)
+        Label4.TabIndex = 5
+        Label4.Text = "nom producto"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(341, 280)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(103, 20)
+        Label5.TabIndex = 6
+        Label5.Text = "prec porducto"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(536, 280)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(108, 20)
+        Label6.TabIndex = 7
+        Label6.Text = "stock producto"
+        ' 
+        ' txtDescripcion
+        ' 
+        txtDescripcion.Location = New Point(99, 323)
+        txtDescripcion.Name = "txtDescripcion"
+        txtDescripcion.ReadOnly = True
+        txtDescripcion.Size = New Size(168, 27)
+        txtDescripcion.TabIndex = 8
+        ' 
+        ' txtPrecio
+        ' 
+        txtPrecio.Location = New Point(303, 323)
+        txtPrecio.Name = "txtPrecio"
+        txtPrecio.ReadOnly = True
+        txtPrecio.Size = New Size(172, 27)
+        txtPrecio.TabIndex = 9
+        ' 
+        ' txtStock
+        ' 
+        txtStock.Location = New Point(508, 323)
+        txtStock.Name = "txtStock"
+        txtStock.ReadOnly = True
+        txtStock.Size = New Size(166, 27)
+        txtStock.TabIndex = 10
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtStock)
+        Controls.Add(txtPrecio)
+        Controls.Add(txtDescripcion)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(btnCargar)
+        Controls.Add(cmbProductos)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents cmbProductos As ComboBox
+    Friend WithEvents btnCargar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtDescripcion As TextBox
+    Friend WithEvents txtPrecio As TextBox
+    Friend WithEvents txtStock As TextBox
+
+End Class
---
+++ EPE1_Kevin_Canales/Form1.vb
@@ -0,0 +1,116 @@
+﻿Imports Microsoft.Office.Interop.Excel
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+    Private productosTable As DataTable
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Configurar el formulario para que no se pueda cambiar de tamaño
+        Me.FormBorderStyle = FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+
+        ' Inicializar el ComboBox vacío
+        cmbProductos.DataSource = Nothing
+    End Sub
+
+    Private Sub btnCargar_Click(sender As Object, e As EventArgs) Handles btnCargar.Click
+        Try
+            Dim filePath As String = "C:\Users\kevin\Downloads\Lista.xls"
+
+            If Not IO.File.Exists(filePath) Then
+                MessageBox.Show("El archivo Lista.xls no se encuentra en la ruta especificada.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End If
+
+            ' Inicializar Excel Interop
+            Dim excelApp As Application = New Application()
+            Dim workbooks As Workbooks = excelApp.Workbooks
+            Dim workbook As Workbook = workbooks.Open(filePath)
+            Dim worksheet As Worksheet = workbook.Sheets(1)
+            Dim range As Range = worksheet.UsedRange
+
+            ' Inicializar productosTable
+            productosTable = New DataTable()
+
+            ' Crear columnas
+            productosTable.Columns.Add("Nombre")
+            productosTable.Columns.Add("Descripcion")
+            productosTable.Columns.Add("Precio")
+            productosTable.Columns.Add("Stock")
+
+            ' Leer filas
+            Dim rowCount As Integer = range.Rows.Count
+
+            If rowCount < 2 Then
+                MessageBox.Show("El archivo Excel no contiene suficientes datos.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                Cleanup(excelApp, workbook)
+                Return
+            End If
+
+            For row As Integer = 2 To rowCount
+                Dim nombre As String = CType(range.Cells(row, 1).Value, String)
+                Dim descripcion As String = CType(range.Cells(row, 2).Value, String)
+                Dim precio As String = CType(range.Cells(row, 3).Value, String)
+                Dim stock As String = CType(range.Cells(row, 4).Value, String)
+
+                ' Verificar si los datos son válidos antes de agregar
+                If String.IsNullOrEmpty(nombre) OrElse String.IsNullOrEmpty(descripcion) OrElse String.IsNullOrEmpty(precio) OrElse String.IsNullOrEmpty(stock) Then
+                    Continue For ' Saltar filas incompletas
+                End If
+
+                productosTable.Rows.Add(nombre, descripcion, precio, stock)
+            Next
+
+            ' Cerrar el libro de trabajo y liberar recursos
+            Cleanup(excelApp, workbook)
+
+            ' Verificar si productosTable tiene datos
+            If productosTable.Rows.Count = 0 Then
+                MessageBox.Show("No se encontraron datos en el archivo Excel.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            End If
+
+            ' Llenar ComboBox
+            cmbProductos.DataSource = productosTable
+            cmbProductos.DisplayMember = "Nombre"
+            cmbProductos.ValueMember = "Nombre"
+
+            ' Deshabilitar el botón después de cargar
+            btnCargar.Enabled = False
+
+            ' Mensaje de éxito
+            MessageBox.Show($"Datos cargados exitosamente. Total de productos: {productosTable.Rows.Count}", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        Catch ex As Exception
+            MessageBox.Show($"Error al cargar el archivo: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub cmbProductos_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbProductos.SelectedIndexChanged
+        If productosTable Is Nothing OrElse productosTable.Rows.Count = 0 Then
+            MessageBox.Show("Los datos del producto no están cargados correctamente.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        If cmbProductos.SelectedIndex >= 0 AndAlso cmbProductos.SelectedIndex < productosTable.Rows.Count Then
+            ' Obtener la fila seleccionada usando el índice del ComboBox
+            Dim selectedRow As DataRow = productosTable.Rows(cmbProductos.SelectedIndex)
+            txtDescripcion.Text = selectedRow("Descripcion").ToString()
+            txtPrecio.Text = selectedRow("Precio").ToString()
+            txtStock.Text = selectedRow("Stock").ToString()
+        Else
+            ' Limpiar los campos si no hay selección válida
+            txtDescripcion.Clear()
+            txtPrecio.Clear()
+            txtStock.Clear()
+        End If
+    End Sub
+
+    Private Sub Cleanup(excelApp As Application, workbook As Workbook)
+        ' Cerrar el libro y liberar recursos
+        workbook.Close(False)
+        Marshal.ReleaseComObject(workbook)
+        excelApp.Quit()
+        Marshal.ReleaseComObject(excelApp)
+    End Sub
+End Class
